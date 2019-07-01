43640e7c96359fd3700adf44eeaa2bfca7b90e2c
Chad Chaney <cchane4@gmail.com>
2019-07-01 10:02:30 -0500

    new file using alert,prompt and variables

---
+++ stalker.js
@@ -0,0 +1,5 @@
+let firstName = prompt("What is your first name?" );
+let lastName = prompt( "What is your last name?");
+let age = prompt("How old are you?"); 
+console.log("My name is " + firstName + " "+ lastName+"." ); 
+console.log( "I am " + age + " years old."); 

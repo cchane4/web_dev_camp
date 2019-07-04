1fc24c34df3356d468ab8fa085cca8b1c5529d96
Chad Chaney <cchane4@gmail.com>
2019-07-04 04:05:51 -0500

    adding the appropriate equal signs

--- imgs/while.js
+++ imgs/while.js
@@ -14,15 +14,19 @@ while (count <= 40){
 }
 
 // another way of printing all even numbers between 10 and 40 
+// a more complete way to check if all numbers are even, instead 
+// of just printing every 2 numbers
 let count  = 10; 
 while (count <=40){  
-    if (count%2 == 0){
+    if (count%2 === 0){
     console.log(count); 
     }
-    count++
+    count+=1
 }
 //print all odd numbers between 300 and 333
-let count  = 301 ;
+// start the counter at 300 and check if all numbers are 
+// NOT divisible by 2
+let count  = 300 ;
 while (count <=333){  
     if (count%2 !== 0){
     console.log(count); 
@@ -34,7 +38,7 @@ while (count <=333){
 // print all numbers divisible by 5 and 3 that are between 5 and 50
 let count  = 5; 
 while (count <=50){
-    if (count%3 == 0 && count%5 == 0 ) {
+    if (count%3 === 0 && count%5 === 0 ) {
     console.log(count); 
     }
     count++

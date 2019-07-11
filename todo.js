18f4da641ad54cf852396d2cfa4c1460dacd3e09
Chad Chaney <cchane4@gmail.com>
2019-07-10 19:31:14 -0500

    adding a while loop to the app

--- todo.js
+++ todo.js
@@ -6,7 +6,20 @@ if (input === "list"){
     console.log(todos); 
 }
 //this is the logic for adding a new todo to the list
+/* if new is typed in the original prompt, prompt again to add a new todo to the list
+then push that typed item to the todos array*/
 else if (input === "new"){ 
    let newTodo = prompt('Add a new todo to the list'); 
    todos.push(newTodo); 
 }
+
+
+
+
+/*condition for the prompt to be repeated as long as 
+the input is not equal to quit */ 
+while(input !== "quit"){ 
+//handle input 
+//ask again for new input
+}
+console.log("ok, you quit the application"); 

92ba59be5024dff6f1d9e8897b03fa0cacb96b82
Chad Chaney <cchane4@gmail.com>
2019-07-10 19:17:15 -0500

    making logic for printing a list to the console

---
+++ todo.js
@@ -0,0 +1,7 @@
+let todos = ["Buy some apples"]; 
+let input = prompt("What would you like to do?"); 
+/* if the input from the prompt is evaluated and found to equal the string "list"
+then, it should print to the console whatever is in the todos array */
+if (input === "list"){ 
+    console.log(todos); 
+}

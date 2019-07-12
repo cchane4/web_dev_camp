29b57ecbdabd9a8e40bbab537fbca66255495cee
Chad Chaney <cchane4@gmail.com>
2019-07-12 13:23:47 -0500

    adding delete functionality

--- todo.js
+++ todo.js
@@ -1,30 +1,36 @@
  
-// window set timeout gives the text on the page a chance to load before the prompt shows up on the screen
 window.setTimeout(() =>  { 
 
     let todos = ["Buy some apples"]; 
 let input = prompt("What would you like to do?");
 
-/*condition for the prompt to be repeated as long as 
-the input is not equal to quit */ 
+
     while(input !== "quit"){ 
-//handle input 
-//ask again for new input
-/* if the input from the prompt is evaluated and found to equal the string "list"
-then, it should print to the console whatever is in the todos array */
+
         if (input === "list"){ 
-         console.log(todos); 
+        console.log("********")
+        // now printing the index 
+        todos.forEach((todo_item, index) => { 
+            console.log(index + ": " + todo_item); 
+        }); 
+        console.log("********")
+
         }
-//this is the logic for adding a new todo to the list
-/* if new is typed in the original prompt, prompt again to add a new todo to the list
-then push that typed item to the todos array*/
+
         else if (input === "new"){ 
             let newTodo = prompt('Add a new todo to the list'); 
             todos.push(newTodo); 
+            console.log("todo has been added"); 
+        }
+
+        else if(input === "delete"){ 
+           let typeIndex = prompt("What is the index of the todo to be deleted?"); 
+           todos.splice(typeIndex, 1); 
+           console.log("todo has been deleted"); 
+
         }
     input = prompt("What would you like to do?"); 
     }
-//this is the logic that will run if quit is input in the prompted text field
 console.log("ok, you quit the application"); 
 
 })

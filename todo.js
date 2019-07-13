7e3c6cc3ba8d1b1665b52caa1316f33f5eb72be6
Chad Chaney <cchane4@gmail.com>
2019-07-13 18:40:19 -0500

    cleaning up the functions inside the loop

--- todo.js
+++ todo.js
@@ -2,35 +2,43 @@
 window.setTimeout(() =>  { 
 
     let todos = ["Buy some apples"]; 
-let input = prompt("What would you like to do?");
+    let input = prompt("What would you like to do?");
 
+let listTodos = () => { 
+    console.log("********"); 
+    // now printing the index 
+    todos.forEach((todo_item, index) => { 
+        console.log(index + ": " + todo_item); 
+    }); 
+    console.log("********"); 
+ }
+
+ let addTodos = () => { 
+    let newTodo = prompt('Add a new todo to the list'); 
+    todos.push(newTodo); 
+    console.log("todo has been added"); 
+ }
+
+ let deleteTodos = () => { 
+    let typeIndex = prompt("What is the index of the todo to be deleted?"); 
+    todos.splice(typeIndex, 1); 
+    console.log("todo has been deleted"); 
+
+ }
 
     while(input !== "quit"){ 
-
         if (input === "list"){ 
-        console.log("********")
-        // now printing the index 
-        todos.forEach((todo_item, index) => { 
-            console.log(index + ": " + todo_item); 
-        }); 
-        console.log("********")
-
+        listTodos();
         }
-
         else if (input === "new"){ 
-            let newTodo = prompt('Add a new todo to the list'); 
-            todos.push(newTodo); 
-            console.log("todo has been added"); 
+         addTodos();    
         }
-
         else if(input === "delete"){ 
-           let typeIndex = prompt("What is the index of the todo to be deleted?"); 
-           todos.splice(typeIndex, 1); 
-           console.log("todo has been deleted"); 
-
+        deleteTodos(); 
         }
     input = prompt("What would you like to do?"); 
     }
 console.log("ok, you quit the application"); 
 
 })
+

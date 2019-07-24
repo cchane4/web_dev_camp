457180772f5b9a329a366774ed34c68f845b6c7c
Chad Chaney <cchane4@gmail.com>
2019-07-24 15:48:02 -0500

    foreach explained

---
+++ foreach.js
@@ -0,0 +1,16 @@
+//a function takes in the array and an anonymous function as arguments
+let myForEach = (arr, func)=> {
+    // loop through the array 
+    for (i = 0; i < arr.length; i++){
+        // call func for each item in array
+    func(arr[i]);
+    } 
+}
+
+let colors = ["red", "yellow","blue"]; 
+
+// color refers to each item in the array
+ myForEach (colors, (color) => { 
+    console.log(color); 
+
+}); 

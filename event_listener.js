843d53c6b47f83322df110985c7c179456c664e2
Chad Chaney <cchane4@gmail.com>
2019-08-27 14:27:38 -0500

    refactoring the toggle code

--- event_listener.js
+++ event_listener.js
@@ -1,11 +1,13 @@
 let button_click = document.querySelector("button");
-let is_green = false;
 
- button_click.addEventListener("click", function(){ 
-     if (is_green){ 
-         document.body.style.background = "yellow"; 
-     }else { 
-    document.body.style.background = "green"; 
-     } 
-     is_green =! is_green; 
-});
+//  button_click.addEventListener("click", function(){ 
+//      if (is_green){ 
+//          document.body.style.background = "yellow"; 
+//      }else { 
+//     document.body.style.background = "green"; 
+//      } 
+//      is_green =! is_green; 
+// });
+button_click.addEventListener("click", function(){ 
+    document.body.classList.toggle("green");
+}); 

a759205e8909ee1cfb28b85fcc7a21230128e9f1
Chad Chaney <cchane4@gmail.com>
2019-09-15 16:30:22 -0500

    adding an echo file to be run server side

---
+++ echo.js
@@ -0,0 +1,10 @@
+// a function that takes an argument and prints the string, num of times
+let echo = (str, num) => { 
+    for (let i = 1; i <= num; i++){ 
+        console.log(str); 
+    }
+  
+}
+
+echo("Echo!", 10)
+echo("Tater Tots", 3)
---
+++ jquery.js
@@ -0,0 +1,4 @@
+$("div").css("background-color","purple"); 
+$(".highlight").css("width","200px"); 
+$("#third").css("border"," 4px orange solid"); 
+$("div:first").css("color","pink"); 
